167f0b2a91bc34cf70465bd19ee13950c562da07
Soulaimane <soulaimanboujnani@gmail.com>
2023-11-27 21:44:22 +0100

    ejericios de 27/11

---
+++ src/javaapplication33/JavaApplication33.java
@@ -0,0 +1,109 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication33;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author dam_boujnani_soulaimane
+ */
+public class JavaApplication33 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+//        Scanner reder = new Scanner(System.in);
+//        int m, n;
+//        System.out.println("Dame el tamaño del vector ");
+//        n = reder.nextInt();
+//        System.out.println("Dame los valores que quieres");
+//        m = reder.nextInt();
+//        int[] tamnaño = new int[n];
+//        Arrays.fill(tamnaño, m);
+//        System.out.println(Arrays.toString(tamnaño));
+      
+
+
+
+
+//
+//        int tamanio = 0, cont = 0, longarray = 17;
+//        for (int i = 0; i <= longarray; i++) {
+//            tamanio += i;
+//        }
+//
+//        int[] valores = new int[tamanio];
+//
+//        for (int i = 1; i <= longarray; i++) {
+//            Arrays.fill(valores, cont, valores.length, i);
+//            cont += i;
+//        }
+//
+//        System.out.println(Arrays.toString(valores));
+//        
+    
+       
+
+
+
+
+
+//
+//        int[] valores = new int[30];
+//        
+//        for (int i = 0; i < valores.length; i++) {
+//            valores[i] = (int) ((Math.random() * (10)));
+//        }
+//        Arrays.sort(valores);
+//        System.out.println(Arrays.toString(valores));
+//    
+    
+    
+//   
+//        String s1 = "prova de text!";
+//        String s2 = "Prova de Text!";
+//        System.out.println("Comprebemos si son iguales");
+//        System.out.println(s1.equals(s2));
+//        System.out.println("comprebemos si son eguales, ahora sin contar las mayusculas");
+//        System.out.println(s1.equalsIgnoreCase(s2));
+//        System.out.println("compremos las consideando el orden alfabetico ");
+//        System.out.println(s2.compareTo(s1)); // da la valor 32 porque segun ASCII la def entre ua letra min y su mayi es 32
+//        
+//        System.out.println("comparemos las considerando el orden alfabetico , sin contar mayusculas");
+//        System.out.println(s1.compareToIgnoreCase(s2));
+    
+   
+
+
+        // Ejemplo 1 :
+    
+//        String s1 = "Papallona";
+//
+//        System.out.println(s1.replace('a', 'e'));
+//        System.out.println(s1);
+//
+//        String s2 = s1.replace('a', 'e');
+        
+
+//        double numero = 3.245777 ;
+//        System.out.println(String.format("%.2f", numero));
+       
+        
+        
+        
+        
+        
+    
+    
+    
+    
+    }
+    
+}
